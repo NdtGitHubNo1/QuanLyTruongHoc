f1e155df3f5e1db67214ca1b14eef69d9ea6da7c
TrieuThuyHang <trieuthuyhang2008@gmail.com>
2021-09-07 20:02:29 +0700

    lan 1

---
+++ QLTH/QLTH/ThongTinGiangDay_Admin.Designer.cs
@@ -0,0 +1,262 @@
+﻿
+namespace QLTH
+{
+     partial class ThongTinGiangDay_Admin
+     {
+          /// <summary> 
+          /// Required designer variable.
+          /// </summary>
+          private System.ComponentModel.IContainer components = null;
+
+          /// <summary> 
+          /// Clean up any resources being used.
+          /// </summary>
+          /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+          protected override void Dispose(bool disposing)
+          {
+               if (disposing && (components != null))
+               {
+                    components.Dispose();
+               }
+               base.Dispose(disposing);
+          }
+
+          #region Component Designer generated code
+
+          /// <summary> 
+          /// Required method for Designer support - do not modify 
+          /// the contents of this method with the code editor.
+          /// </summary>
+          private void InitializeComponent()
+          {
+               this.dataGridView1 = new System.Windows.Forms.DataGridView();
+               this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+               this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+               this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+               this.labelControl4 = new DevExpress.XtraEditors.LabelControl();
+               this.labelControl5 = new DevExpress.XtraEditors.LabelControl();
+               this.labelControl6 = new DevExpress.XtraEditors.LabelControl();
+               this.btnThem = new DevExpress.XtraEditors.SimpleButton();
+               this.btnSua = new DevExpress.XtraEditors.SimpleButton();
+               this.btnXoa = new DevExpress.XtraEditors.SimpleButton();
+               this.btnLuu = new DevExpress.XtraEditors.SimpleButton();
+               this.btnTimKiem = new DevExpress.XtraEditors.SimpleButton();
+               this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+               this.textEdit2 = new DevExpress.XtraEditors.TextEdit();
+               this.textEdit3 = new DevExpress.XtraEditors.TextEdit();
+               this.textEdit4 = new DevExpress.XtraEditors.TextEdit();
+               this.textEdit5 = new DevExpress.XtraEditors.TextEdit();
+               this.textEdit6 = new DevExpress.XtraEditors.TextEdit();
+               ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+               ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+               ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).BeginInit();
+               ((System.ComponentModel.ISupportInitialize)(this.textEdit3.Properties)).BeginInit();
+               ((System.ComponentModel.ISupportInitialize)(this.textEdit4.Properties)).BeginInit();
+               ((System.ComponentModel.ISupportInitialize)(this.textEdit5.Properties)).BeginInit();
+               ((System.ComponentModel.ISupportInitialize)(this.textEdit6.Properties)).BeginInit();
+               this.SuspendLayout();
+               // 
+               // dataGridView1
+               // 
+               this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+               this.dataGridView1.Location = new System.Drawing.Point(24, 54);
+               this.dataGridView1.Name = "dataGridView1";
+               this.dataGridView1.RowHeadersWidth = 51;
+               this.dataGridView1.RowTemplate.Height = 24;
+               this.dataGridView1.Size = new System.Drawing.Size(805, 612);
+               this.dataGridView1.TabIndex = 0;
+               // 
+               // labelControl1
+               // 
+               this.labelControl1.Appearance.Font = new System.Drawing.Font("Sitka Text", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+               this.labelControl1.Appearance.ForeColor = System.Drawing.Color.Brown;
+               this.labelControl1.Appearance.Options.UseFont = true;
+               this.labelControl1.Appearance.Options.UseForeColor = true;
+               this.labelControl1.Location = new System.Drawing.Point(722, 0);
+               this.labelControl1.Name = "labelControl1";
+               this.labelControl1.Size = new System.Drawing.Size(324, 48);
+               this.labelControl1.TabIndex = 1;
+               this.labelControl1.Text = "Thông tin giảng dạy";
+               // 
+               // labelControl2
+               // 
+               this.labelControl2.Location = new System.Drawing.Point(962, 130);
+               this.labelControl2.Name = "labelControl2";
+               this.labelControl2.Size = new System.Drawing.Size(39, 17);
+               this.labelControl2.TabIndex = 2;
+               this.labelControl2.Text = "Mã lớp";
+               // 
+               // labelControl3
+               // 
+               this.labelControl3.Location = new System.Drawing.Point(962, 197);
+               this.labelControl3.Name = "labelControl3";
+               this.labelControl3.Size = new System.Drawing.Size(72, 16);
+               this.labelControl3.TabIndex = 3;
+               this.labelControl3.Text = "Mã giáo viên";
+               // 
+               // labelControl4
+               // 
+               this.labelControl4.Location = new System.Drawing.Point(962, 268);
+               this.labelControl4.Name = "labelControl4";
+               this.labelControl4.Size = new System.Drawing.Size(59, 17);
+               this.labelControl4.TabIndex = 4;
+               this.labelControl4.Text = "Ngày dạy";
+               // 
+               // labelControl5
+               // 
+               this.labelControl5.Location = new System.Drawing.Point(962, 338);
+               this.labelControl5.Name = "labelControl5";
+               this.labelControl5.Size = new System.Drawing.Size(39, 17);
+               this.labelControl5.TabIndex = 5;
+               this.labelControl5.Text = "Số tiết";
+               // 
+               // labelControl6
+               // 
+               this.labelControl6.Location = new System.Drawing.Point(962, 416);
+               this.labelControl6.Name = "labelControl6";
+               this.labelControl6.Size = new System.Drawing.Size(94, 17);
+               this.labelControl6.TabIndex = 6;
+               this.labelControl6.Text = "Số tiền một tiết";
+               // 
+               // btnThem
+               // 
+               this.btnThem.Appearance.ForeColor = System.Drawing.Color.Black;
+               this.btnThem.Appearance.Options.UseForeColor = true;
+               this.btnThem.Location = new System.Drawing.Point(1007, 486);
+               this.btnThem.Name = "btnThem";
+               this.btnThem.Size = new System.Drawing.Size(94, 29);
+               this.btnThem.TabIndex = 7;
+               this.btnThem.Text = "Thêm";
+               // 
+               // btnSua
+               // 
+               this.btnSua.Location = new System.Drawing.Point(1183, 486);
+               this.btnSua.Name = "btnSua";
+               this.btnSua.Size = new System.Drawing.Size(94, 29);
+               this.btnSua.TabIndex = 8;
+               this.btnSua.Text = "Sửa";
+               // 
+               // btnXoa
+               // 
+               this.btnXoa.Location = new System.Drawing.Point(1348, 486);
+               this.btnXoa.Name = "btnXoa";
+               this.btnXoa.Size = new System.Drawing.Size(94, 29);
+               this.btnXoa.TabIndex = 9;
+               this.btnXoa.Text = "Xóa";
+               // 
+               // btnLuu
+               // 
+               this.btnLuu.Location = new System.Drawing.Point(1183, 548);
+               this.btnLuu.Name = "btnLuu";
+               this.btnLuu.Size = new System.Drawing.Size(94, 29);
+               this.btnLuu.TabIndex = 10;
+               this.btnLuu.Text = "Lưu";
+               // 
+               // btnTimKiem
+               // 
+               this.btnTimKiem.Location = new System.Drawing.Point(1348, 618);
+               this.btnTimKiem.Name = "btnTimKiem";
+               this.btnTimKiem.Size = new System.Drawing.Size(94, 29);
+               this.btnTimKiem.TabIndex = 11;
+               this.btnTimKiem.Text = "Tìm kiếm";
+               // 
+               // textEdit1
+               // 
+               this.textEdit1.Location = new System.Drawing.Point(1063, 124);
+               this.textEdit1.Name = "textEdit1";
+               this.textEdit1.Size = new System.Drawing.Size(293, 22);
+               this.textEdit1.TabIndex = 12;
+               // 
+               // textEdit2
+               // 
+               this.textEdit2.Location = new System.Drawing.Point(1063, 194);
+               this.textEdit2.Name = "textEdit2";
+               this.textEdit2.Size = new System.Drawing.Size(293, 22);
+               this.textEdit2.TabIndex = 13;
+               // 
+               // textEdit3
+               // 
+               this.textEdit3.Location = new System.Drawing.Point(1063, 410);
+               this.textEdit3.Name = "textEdit3";
+               this.textEdit3.Size = new System.Drawing.Size(293, 22);
+               this.textEdit3.TabIndex = 14;
+               // 
+               // textEdit4
+               // 
+               this.textEdit4.Location = new System.Drawing.Point(1063, 335);
+               this.textEdit4.Name = "textEdit4";
+               this.textEdit4.Size = new System.Drawing.Size(293, 22);
+               this.textEdit4.TabIndex = 15;
+               // 
+               // textEdit5
+               // 
+               this.textEdit5.Location = new System.Drawing.Point(1063, 265);
+               this.textEdit5.Name = "textEdit5";
+               this.textEdit5.Size = new System.Drawing.Size(293, 22);
+               this.textEdit5.TabIndex = 16;
+               // 
+               // textEdit6
+               // 
+               this.textEdit6.Location = new System.Drawing.Point(1007, 622);
+               this.textEdit6.Name = "textEdit6";
+               this.textEdit6.Size = new System.Drawing.Size(335, 22);
+               this.textEdit6.TabIndex = 17;
+               // 
+               // ThongTinGiangDay_Admin
+               // 
+               this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+               this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+               this.Controls.Add(this.textEdit6);
+               this.Controls.Add(this.textEdit5);
+               this.Controls.Add(this.textEdit4);
+               this.Controls.Add(this.textEdit3);
+               this.Controls.Add(this.textEdit2);
+               this.Controls.Add(this.textEdit1);
+               this.Controls.Add(this.btnTimKiem);
+               this.Controls.Add(this.btnLuu);
+               this.Controls.Add(this.btnXoa);
+               this.Controls.Add(this.btnSua);
+               this.Controls.Add(this.btnThem);
+               this.Controls.Add(this.labelControl6);
+               this.Controls.Add(this.labelControl5);
+               this.Controls.Add(this.labelControl4);
+               this.Controls.Add(this.labelControl3);
+               this.Controls.Add(this.labelControl2);
+               this.Controls.Add(this.labelControl1);
+               this.Controls.Add(this.dataGridView1);
+               this.Name = "ThongTinGiangDay_Admin";
+               this.Size = new System.Drawing.Size(1549, 681);
+               ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+               ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+               ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).EndInit();
+               ((System.ComponentModel.ISupportInitialize)(this.textEdit3.Properties)).EndInit();
+               ((System.ComponentModel.ISupportInitialize)(this.textEdit4.Properties)).EndInit();
+               ((System.ComponentModel.ISupportInitialize)(this.textEdit5.Properties)).EndInit();
+               ((System.ComponentModel.ISupportInitialize)(this.textEdit6.Properties)).EndInit();
+               this.ResumeLayout(false);
+               this.PerformLayout();
+
+          }
+
+          #endregion
+
+          private System.Windows.Forms.DataGridView dataGridView1;
+          private DevExpress.XtraEditors.LabelControl labelControl1;
+          private DevExpress.XtraEditors.LabelControl labelControl2;
+          private DevExpress.XtraEditors.LabelControl labelControl3;
+          private DevExpress.XtraEditors.LabelControl labelControl4;
+          private DevExpress.XtraEditors.LabelControl labelControl5;
+          private DevExpress.XtraEditors.LabelControl labelControl6;
+          private DevExpress.XtraEditors.SimpleButton btnThem;
+          private DevExpress.XtraEditors.SimpleButton btnSua;
+          private DevExpress.XtraEditors.SimpleButton btnXoa;
+          private DevExpress.XtraEditors.SimpleButton btnLuu;
+          private DevExpress.XtraEditors.SimpleButton btnTimKiem;
+          private DevExpress.XtraEditors.TextEdit textEdit1;
+          private DevExpress.XtraEditors.TextEdit textEdit2;
+          private DevExpress.XtraEditors.TextEdit textEdit3;
+          private DevExpress.XtraEditors.TextEdit textEdit4;
+          private DevExpress.XtraEditors.TextEdit textEdit5;
+          private DevExpress.XtraEditors.TextEdit textEdit6;
+     }
+}
---
+++ QLTH/QLTH/ThongTinGiangDay_Admin.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLTH
+{
+     public partial class ThongTinGiangDay_Admin : DevExpress.XtraEditors.XtraUserControl
+     {
+          public ThongTinGiangDay_Admin()
+          {
+               InitializeComponent();
+          }
+     }
+}
